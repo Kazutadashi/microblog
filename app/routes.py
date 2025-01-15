3f77ec311fdd666158957687103d3d5c71dd531e
Kazutadashi <owenvl@yahoo.com>
2025-01-14 19:38:44 -0700

    Add new index template

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 # This is called a route. Routes are responsible for determining
@@ -6,4 +7,21 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+	# create some mock objects to build a template around
+	user = {'username': 'The best girl'}
+	posts = [
+		{
+			'author': {'username': 'Owen'},
+			'body': {'Evelyn is the best'},
+		},
+		{
+			'author': {'username': 'Evelyn'},
+			'body': {'Fun day with Owen in Moab!'},
+		}
+	]
+
+	# templates are the actual html documents, and they must be rendered
+	# to be visible tok the user. When we direct user to a function view
+	# the view can try and load a template to provide to the user through
+	# the render template function
+	return render_template('index.html', title='Home', user=user)
