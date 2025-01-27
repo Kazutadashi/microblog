1a5aa03f2ffd48088eb5b349fd8d485c65ae2ee4
Kazutadashi <owenvl@yahoo.com>
2025-01-26 20:01:34 -0700

    Add profile editor

--- app/models.py
+++ app/models.py
@@ -5,8 +5,11 @@ from flask_login import UserMixin
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
+from hashlib import md5
 
 # comes from the LoginManager object
+# this function is called everytime a request is made that requires
+# current_user. It then populates this information into current_user
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -22,7 +25,8 @@ class User(UserMixin, db.Model):
     email:          so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash:  so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts:          so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
-
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -32,12 +36,18 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}'.format(self.username)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
 class Post(db.Model):
     id:         so.Mapped[int] = so.mapped_column(primary_key=True)
     body:       so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp:  so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
     user_id:    so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     author:     so.Mapped[User] = so.relationship(back_populates='posts')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,19 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
-from app.forms import RegistrationForm
+from app.forms import RegistrationForm, EditProfileForm
 import sqlalchemy as sa
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
+from datetime import datetime, timezone
+
+
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.now(timezone.utc)
+		db.session.commit()
+
 
 # This is called a route. Routes are responsible for determining
 # what happens when a visitor goes to a specific place on your website
@@ -80,3 +89,31 @@ def register():
 		flash('Congratulations, you are now a registered user!')
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = db.first_or_404(sa.select(User).where(User.username == username))
+	posts = [
+		{'author': user, 'body': 'Test post #1'},
+		{'author': user, 'body': 'Test post #2'}
+	]
+	return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit_profile'))
+	# when a user simply just visits this page, it becomes a GET request
+	# so this part of the code runs instead of the POST part
+	elif request.method == 'GET':
+		# this fills the form data automatically with what existed in the database
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', title='Edit Profile', form=form)
