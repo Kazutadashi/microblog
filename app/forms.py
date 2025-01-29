f9efbc1f4326d985d756ac653429b532c8363082
Kazutadashi <owenvl@yahoo.com>
2025-01-28 17:50:35 -0700

    Fix crash due to two people editing username to the same name

--- app/forms.py
+++ app/forms.py
@@ -38,3 +38,17 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    # this function is called by Flask-WTF anytime submit is used as a POST request
+    # this runs after all the other validators we've defined earlier.
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data
+            ))
+            if user is not None:
+                raise ValidationError("Please use a different username.")
