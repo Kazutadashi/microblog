c90a30719f0487ed96f276d245f3fd765727b600
Kazutadashi <owenvl@yahoo.com>
2025-02-08 23:48:57 -0700

    Add pagination logic

--- app/routes.py
+++ app/routes.py
@@ -39,8 +39,10 @@ def bobsanchez():
 		# and helps prevent duplicated data
 		return redirect(url_for('bobsanchez'))
 
-	posts = db.session.scalars(current_user.following_posts()).all()
-
+	page = request.args.get('page', 1, type=int)
+	posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+	next_url = url_for('bobsanchez', page=posts.next_num) if posts.has_next else None
+	prev_url = url_for('bobsanchez', page=posts.prev_num) if posts.has_prev else None
 	# templates are the actual html documents, and they must be rendered
 	# to be visible tok the user. When we direct user to a function view
 	# the view can try and load a template to provide to the user through
@@ -165,6 +167,8 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+	page = request.args.get('page', 1, type=int)
 	query = sa.select(Post).order_by(Post.timestamp.desc())
-	posts = db.session.scalars(query).all()
-	return render_template('index.html', title='Explore', posts=posts)
+	posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+
+	return render_template('index.html', title='Explore', posts=posts.items)
