4ba243b9d27bd17d64ed679ee63979e7f425d5c1
Owen L <owenvl@yahoo.com>
2025-03-05 00:02:32 -0700

    Add translation function that uses Azure translator API services

---
+++ app/translate.py
@@ -0,0 +1,69 @@
+from flask_babel import _
+from app import app
+import requests, uuid, json
+
+# def translate_old(text, source_language, dest_language):
+#
+#     if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+#         return _('Error: the translation service is not configured.')
+#     auth = {
+#         'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY'],
+#         'Ocp-Apim-Subscription-Region': 'westus2',
+#     }
+#     r = requests.post(
+#         'https://api.cognitive.microsofttranslator.com/'
+#         'translate?-api-version=3.0&from={}&to={}'.format(
+#             source_language, dest_language
+#         ),
+#         headers=auth,
+#         json=[{'Text': text}]
+#     )
+#     if r.status_code != 200:
+#         print(r.status_code)
+#         print(r)
+#         return _('Error: the translation service failed.')
+#     return r.json()[0]['translations'][0]['text']
+
+def translate(text, source_language, dest_language):
+
+    if 'MS_TRANSLATOR_KEY' not in app.config or not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+
+    # Add your key and endpoint
+    key = app.config['MS_TRANSLATOR_KEY']
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+
+    # location, also known as region.
+    # required if you're using a multi-service or regional (not global) resource. It can be found in the Azure portal on the Keys and Endpoint page.
+    location = "westus2"
+
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': dest_language
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        # location required if you're using a multi-service or regional (not global) resource.
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text': text
+    }]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+
+    if request.status_code != 200:
+        return _('Error: the translation service failed.')
+
+    response = request.json()
+    # return the actual translation as a string
+    return response[0]['translations'][0]['text']
--- config.py
+++ config.py
@@ -15,3 +15,4 @@ class Config:
     FUNNY = os.environ.get('FUNNY')
     POSTS_PER_PAGE = 10
     LANGUAGES = ['en', 'zh', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
