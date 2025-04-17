08c2c1499db003ba409a6f137376f3cf8f0b6c4a
Owen L <owenvl@yahoo.com>
2025-04-17 00:09:02 -0600

    Add polling for notifications

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,7 +3,7 @@ from app import db
 from app.main import bp
 from app.main.forms import MessageForm
 from app.translate import translate
-from app.models import User, Post, Message
+from app.models import User, Post, Message, Notification
 from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm
 import sqlalchemy as sa
 from flask_login import current_user, login_required
@@ -190,7 +190,7 @@ def send_message(recipient):
 @login_required
 def messages():
 	current_user.last_message_read_time = datetime.now(timezone.utc)
-	current_user.add_notification()
+	current_user.add_notification('unread_message_count', 0)
 	db.session.commit()
 	page = request.args.get('page', 1, type=int)
 	query = current_user.messages_received.select().order_by(
@@ -201,3 +201,20 @@ def messages():
 	next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
 	prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
 	return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+	since = request.args.get('since', 0.0, type=float)
+	query = current_user.notifications.select().where(
+		Notification.timestamp > since
+	).order_by(Notification.timestamp.asc())
+	notifications = db.session.scalars(query)
+	return [
+		{
+			'name': n.name,
+			'data': n.get_data(),
+			'timestamp': n.timestamp
+		}
+		for n in notifications
+	]
