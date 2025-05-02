7be59e65a4ab55f0737082d8c4b2364d46a2ffce
Owen L <owenvl@yahoo.com>
2025-05-01 22:37:48 -0600

    Add User create method for API

--- app/models.py
+++ app/models.py
@@ -228,6 +228,13 @@ class User(UserMixin, db.Model):
             data['email'] = self.email
         return data
 
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
 class Post(SearchableMixin, db.Model):
     id:         so.Mapped[int] = so.mapped_column(primary_key=True)
     body:       so.Mapped[str] = so.mapped_column(sa.String(140))
