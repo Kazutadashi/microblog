cd559a21d2f9ce2afebfa683addb444017d04396
Owen L <owenvl@yahoo.com>
2025-05-12 07:14:49 -0600

    Add get_following api end point

--- app/api/users.py
+++ app/api/users.py
@@ -1,6 +1,8 @@
 from app.api import bp
 from app.models import User
 from app import db
+import sqlalchemy as sa
+from flask import request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -8,15 +10,24 @@ def get_user(id):
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return User.to_collection_dict(sa.select(User), page, per_page, 'api.get_users')
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
-    pass
+    user = db.get_or_404(User, id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return User.to_collection_dict(user.followers.select(), page, 'api.get_followers', id=id)
+
 
 @bp.route('/users/<int:id>/following', methods=['GET'])
 def get_following(id):
-    pass
+    user = db.get_or_404(User, id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    return User.to_collection_dict(user.following.select(), page, per_page, 'api.get_following', id=id)
 
 @bp.route('/users', methods=['POST'])
 def create_user():
