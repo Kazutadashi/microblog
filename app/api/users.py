63acd34bcb13375fac2349359f523fb5bdec0d3c
Owen L <owenvl@yahoo.com>
2025-05-12 19:26:48 -0600

    Add user creation functionality to API

--- app/api/users.py
+++ app/api/users.py
@@ -3,6 +3,8 @@ from app.models import User
 from app import db
 import sqlalchemy as sa
 from flask import request
+from flask import url_for
+from app.api.errors import bad_request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
@@ -19,7 +21,7 @@ def get_followers(id):
     user = db.get_or_404(User, id)
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
-    return User.to_collection_dict(user.followers.select(), page, 'api.get_followers', id=id)
+    return User.to_collection_dict(user.followers.select(), page, per_page, 'api.get_followers', id=id)
 
 
 @bp.route('/users/<int:id>/following', methods=['GET'])
@@ -31,7 +33,18 @@ def get_following(id):
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json()
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email, and password fields')
+    if db.session.scalar(sa.select(User).where(User.username == data['username'])):
+        return bad_request('please use a different username')
+    if db.session.scalar(sa.select(User).where(User.email == data['email'])):
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    return user.to_dict(), 201, {'Location': url_for('api.get_user', id=user.id)}
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
