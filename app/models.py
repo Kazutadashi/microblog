69a10076ce4ec95c1aaeeadb347ba0a15b27eb87
Owen L <kazutadashi@yahoo.com>
2025-05-25 17:46:41 -0600

    Add Task model

--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ from time import time
 import json
 import jwt
 import secrets
+import redis
+import rq
 
 # comes from the LoginManager object
 # this function is called everytime a request is made that requires
@@ -136,6 +138,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     messages_received: so.WriteOnlyMapped['Message'] = so.relationship(
         foreign_keys='Message.recipient_id', back_populates='recipient'
     )
+    tasks: so.WriteOnlyMapped['Task'] = so.relationship(back_populates='user')
+
 
     def add_notification(self, name, data):
         db.session.execute(self.notifications.delete().where(
@@ -281,6 +285,7 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
             return None
         return user
 
+
 class Post(SearchableMixin, db.Model):
     id:         so.Mapped[int] = so.mapped_column(primary_key=True)
     body:       so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -294,6 +299,7 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
 
 class Message(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -313,6 +319,7 @@ class Message(db.Model):
     def __repr__(self):
         return '<Message {}>'.format(self.body)
 
+
 class Notification(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key = True)
     name: so.Mapped[str] = so.mapped_column(sa.String(128), index=True)
@@ -324,6 +331,26 @@ class Notification(db.Model):
 
     def get_data(self):
         return json.loads(str(self.payload_json))
+
+
+class Task(db.Model):
+    id: so.Mapped[int] = so.mapped_column(sa.String(36), primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(128), index=True)
+    description: so.Mapped[Optional[str]] = so.mapped_column(sa.String(128))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    complete: so.Mapped[bool] = so.mapped_column(default=False)
+    user: so.Mapped[User] = so.relationship(back_populates='tasks')
+
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
 
 
 
