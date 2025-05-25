2e7d48afb6913c837016396bdb4ca1a60f74395d
Owen L <kazutadashi@yahoo.com>
2025-05-24 23:28:03 -0600

    Update example

--- app/tasks.py
+++ app/tasks.py
@@ -1,8 +1,14 @@
 import time
+from rq import get_current_job
 
 def example(seconds):
-    print('starting task')
+    job = get_current_job()
+    print('Starting task')
     for i in range(seconds):
+        job.meta['progress'] = 100.0 * i / seconds
+        job.save_meta()
         print(i)
         time.sleep(1)
-    print('task completed')
+    job.meta['progress'] = 100
+    job.save_meta()
+    print('Task complete')
